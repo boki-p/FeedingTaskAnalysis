f6f7fbb244f969f4c19be17d54de74a63179386d
boki-p <boki9201@gmail.com>
2018-12-12 17:06:03 -0700

    Update Animation_FeedingTask.m
    Included animation of the four markers. This way we have another visualization of when do markers occur

--- Animation_FeedingTask.m
+++ Animation_FeedingTask.m
@@ -1,10 +1,15 @@
 % Left panel hand trajectory, right panel resultant velocity
 
+
 % not gonna play the whole thing
 TIME_RATIO = 1;
 % Play every other frame to set video speed to 2x real speed
-indices = 1:4:TIME_RATIO*length(t);
+indices = 1:2:TIME_RATIO*length(t);
 
+% Recently added, to see if can show markers on the animation too
+idx_all = sort([idx_endoftransport; idx_peak; idx_scoopstart; idx_startofreach]);
+
+%%
 figure
 subplot(1,2,1)
 plot(pos_matrix(indices,1),pos_matrix(indices,2))
@@ -29,32 +34,53 @@ sub1 = subplot(1,2,1);
 axis([-0.4 0.19 -0.03 0.2])
 sub1.Position = [0.1,0.2,0.35,0.60];
 sub1.Color = 'none';
-sub1.XColor = 'none'
-sub1.YColor = 'none'
-title(sub1,'Hand Trajectory')
+sub1.XColor = 'none';
+sub1.YColor = 'none';
+title(sub1,'Hand Trajectory');
 a_task = animatedline('Color','black','LineWidth',2);
+hold on 
+plot1 = plot(0, 0, '*', 'MarkerFaceColor', 'y', 'MarkerEdgeColor', 'b')
 
 sub2 = subplot(1,2,2);
-axis([0 t(end) -1.5 0.8])
+axis([0 t(end) -1.5 0.8]);
 sub2.Position = [0.5,0.2,0.48,0.60];
 sub2.Color = 'none';
-%sub2.XColor = 'none'
-sub2.YColor = 'none'
+%sub2.XColor = 'none';
+sub2.YColor = 'none';
 title(sub2,'Velocity Profile')
 a_vel = animatedline('Color','black','LineWidth',2);
+hold on
+plot2 = plot(0, 0, '*', 'MarkerFaceColor', 'y', 'MarkerEdgeColor', 'b')
 
 cnt = 0;
+
+count = 1;
 
 for k = 1:length(indices)
     % first line
     xk = x_task(indices(k));
     yk = y_task(indices(k));
     addpoints(a_task,xk,yk);
-
     % second line
     tk = t(indices(k));
     vk = v_vel(indices(k));
     addpoints(a_vel,tk,vk);
+    
+    % check if this is frame has marker
+    if (indices(k) - idx_all(count) <= 2) & (indices(k) - idx_all(count) >= -2)
+         % Then on top of the animation, plot the dots on there 
+        plot1.XData = xk; 
+        plot1.YData = yk; 
+        % second line
+        plot2.XData = tk;
+        plot2.YData = vk;
+        % then move to the next 
+        count = count + 1;
+        % when count is oversize, reset to 1
+        if count == n_reps * 4
+            count = 1;
+        end
+    end
 
     % update screen
     drawnow 
